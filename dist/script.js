3ebcf1558a7c6fa20851a594967b9a327cfd5aaa
sullyjs <jsylvir@gmail.com>
2021-04-10 15:10:50 +0200

    edit

--- dist/script.js
+++ dist/script.js
@@ -17,13 +17,6 @@ function incrementNumber() {
     points.innerHTML = number;
 }
 
-function decrementNumber() {
-    var points = document.getElementById('points');
-    var number = points.innerHTML;
-    number-=25;
-    points.innerHTML = number;
-}
-
 function getRandomKey() {
   return keys[getRandomNumber(0, keys.length-1)]
 }
@@ -48,16 +41,14 @@ document.addEventListener("keyup", event => {
     keyElement.classList.remove("hit")
   })
   
-    if (keyPressed === highlightedKey.innerHTML) {
-        timestamps.unshift(getTimestamp());
-        const elapsedTime = timestamps[0] - timestamps[1];
-        console.log(`Character per minute ${60 / elapsedTime}`)
-        highlightedKey.classList.remove("selected");
-        incrementNumber();
-        targetRandomKey();
-    } else {
-        decrementNumber();
-    }
+  if (keyPressed === highlightedKey.innerHTML) {
+    timestamps.unshift(getTimestamp());
+    const elapsedTime = timestamps[0] - timestamps[1];
+    console.log(`Character per minute ${60/elapsedTime}`)
+      highlightedKey.classList.remove("selected");
+      incrementNumber();
+    targetRandomKey();
+  } 
 })
 
 targetRandomKey();
